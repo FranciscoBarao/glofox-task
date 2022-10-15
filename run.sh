ec375cbdde9b2ef959e1a3e87125f7edd5d37545
FranciscoBarao <s.franciscobarao@gmail.com>
2022-10-15 19:12:34 +0100

    Changed run.sh to run DB first and then the service. DB connection was failing because it was not ready. Quick fix and should be improved. Changed postgres to alpine version

--- run.sh
+++ run.sh
@@ -2,8 +2,11 @@
 docker-compose down
 
 # Brings services up
-docker-compose up -d --build 
+docker-compose up -d --build glofox-task-db
 
+sleep 5 # Database was not ready when gorm attempted to connect
+
+docker-compose up -d --build glofox-task
 
 
 #Docker logging
